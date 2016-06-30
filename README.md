85c181df98a0622306ffb116388db45d2a3c8706
alex-pro <a.partitskiy@gmail.com>
2016-06-30 18:58:19 +0300

    add readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# Pagination for [Twitter Bootstrap 3](http://getbootstrap.com/components/#pagination)
+# Setup
 
-With support for:
+Write in terminal:
 
-- erb
-- haml
-- slim
+- git clone git@github.com:alex-pro/rails-file-hosting.git
+- cd rails-file-hosting
+- bundle install
+- bundle exec rake db:create db:migrate
+- bundle exec rake sunspot:solr:start
+- rails s
+
+Open in your browser [localhost:3000](http://localhost:3000/).
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150702091553) do
 
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "folders", force: :cascade do |t|
     t.text     "name",       limit: 65535
     t.integer  "user_id",    limit: 4
